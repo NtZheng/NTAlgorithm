9b0939a73ce76d6830f122b3b4ea655a134ca985
nineteen <zhengzhen03@meituan.com>
2018-01-10 18:14:12 +0800

    完成了“最近公共父节点”

--- 最近公共父节点/最近公共父节点/main.cpp
+++ 最近公共父节点/最近公共父节点/main.cpp
@@ -8,8 +8,49 @@
 
 #include <iostream>
 
+using namespace std;
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+    val(x), left(NULL), right(NULL) {
+    }
+};
+
+// 找最近公共父节点
+TreeNode* findClosestParent(TreeNode *A, TreeNode *B, TreeNode *root) {
+    if (root == NULL) {
+        return NULL;
+    }
+    
+    if (root == A || root == B) {
+        return root;
+    }
+    
+    TreeNode *leftNode = findClosestParent(A, B, root->left);
+    TreeNode *rightNode = findClosestParent(A, B, root->right);
+
+    if (leftNode != NULL && rightNode != NULL) {
+        return root;
+    }
+    
+    if (leftNode != NULL) {
+        return leftNode;
+    }
+    
+    if (rightNode != NULL) {
+        return rightNode;
+    }
+    
+    return NULL;
+}
+
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
