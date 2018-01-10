afff22b85aa5dd04e5045f53c0b14179278e6ee2
nineteen <zhengzhen03@meituan.com>
2018-01-10 16:39:20 +0800

    开始编辑“最近公共父节点”

---
+++ 最近公共父节点/最近公共父节点/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  最近公共父节点
+//
+//  Created by 郑祯 on 10/01/2018.
+//  Copyright © 2018 meituan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
